32f1f9224590ab5c9269bcabf6d49383e1e194d9
angelo-soyannwo <angelosoyannwo@gmail.com>
2022-02-12 12:01:56 -0500

    p5 commented

--- Chapter 4/p5.c
+++ Chapter 4/p5.c
@@ -4,7 +4,8 @@
 
 int main() {
 	int array [10];
-	
+
+//generate a sorted set of contiguous data	
 	int counter = 0;
 	for(int j=1; j<5; j++){
 		for (int z=1; z<(j+1); z++){
@@ -16,6 +17,7 @@ int main() {
 	for(int k=0; k<10; k++)
 		printf("%d\n", array[k]);
 
+//step through logic for the algorithm where the number of elements in the array is 10 
 	int n = 10;
 	int var = array[0];
 	int current_count = 0;
